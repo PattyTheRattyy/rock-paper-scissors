2d3cb33605a456f62833ad2f9055b595ab0213e2
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-07-28 23:27:34 -0400

    Add function to prompt user for their choice
    Created a function called getHumanChoice() which prompts the user for their choice in the game of rock paper scissors. Validates input and rejects anything that isnt rock paper or scissors. It is case insensitive as inputs are converted to lower case before strict comparison.

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ function getComputerChoice() {
     /* generate random number */
     let choice = Math.floor(Math.random()*3);
 
+    // remove later
+    console.log(choice);
+
     /* map random number to a choice */
     if (choice === 0) {
         return "rock";
@@ -32,3 +35,27 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+
+function getHumanChoice() {
+    /* get user input */
+   let input = prompt("What is your choice?", "either rock, paper, or scissors");
+
+   if (input.toLowerCase() === "rock") {
+    return "rock"
+   }
+   else if (input.toLowerCase() === "paper") {
+    return "paper"
+   }
+   else if (input.toLowerCase() === "scissors") {
+    return "scissors"
+   }
+
+   /* if it doesnt match one of the options, prompt the user again */
+   else {
+    return getHumanChoice();
+   }
+
+}
+
+console.log(getHumanChoice());
