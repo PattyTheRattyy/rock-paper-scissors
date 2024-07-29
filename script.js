4807f88c94a73c1a068865aa325486d6e1a53c35
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-07-29 19:04:34 -0400

    Add function to play a round of the game
    Added the playRound() function which takes in the result of getHumanChoice() and getComputerChoice() as arguments to the function. It then determines who won and outputs the result to the console and incremements the score for the correct player.

--- script.js
+++ script.js
@@ -30,8 +30,6 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 
 function getHumanChoice() {
     /* get user input */
@@ -54,4 +52,61 @@ function getHumanChoice() {
 
 }
 
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+
+    // determine winner with conditionals based off the arguments
+    // output result to console
+    // increment score
+
+    // human chooses rock
+    if (humanChoice === "rock" && computerChoice === "rock") {
+       console.log("Tie! You both chose Rock.");
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock.");
+        computerScore += 1;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors.");
+        humanScore += 1;
+    }
+
+    // human chooses paper    
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock.");
+        humanScore += 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "paper") {
+        console.log("Tie! You both chose Paper.");
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beat Paper.");
+        computerScore += 1;
+    }
+
+    // human chooses scissors
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats scissors.");
+        computerScore += 1;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beat Paper.");
+        humanScore += 1;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "scissors") {
+        console.log("Tie! You both chose Scissors.");
+    }
+
+    else {
+        console.log("Error, wrong or no choice given.");
+    }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
